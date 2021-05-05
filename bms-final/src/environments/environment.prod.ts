db78676bafad5826f4b46ac0bd4ca61f13c57e9f
vishalpatil7860 <37109774+vishalpatil7860@users.noreply.github.com>
2021-05-05 15:05:33 +0530

    Add files via upload

---
+++ bms-final/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true.valueOf,
+  firebase: {
+    apiKey: "AIzaSyAoMYubVFA6imbP2HctxxFfzUob_uuU1v8",
+    authDomain: "bms-app-2f416.firebaseapp.com",
+    databaseURL: "https://bms-app-2f416-default-rtdb.firebaseio.com",
+    projectId: "bms-app-2f416",
+    storageBucket: "bms-app-2f416.appspot.com",
+    messagingSenderId: "364454411617",
+    appId: "1:364454411617:web:bd3e0d4849434cd9885920"
+  }
+};

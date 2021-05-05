02f3b77b95175f205c2c315dc7d867d6cc614bc7
vishalpatil7860 <37109774+vishalpatil7860@users.noreply.github.com>
2021-05-05 14:52:07 +0530

    Add files via upload

---
+++ bms-final/src/app/auth/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  
+  constructor(private authService: AuthService) { }
+  
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl('', { validators: [Validators.required] })
+    });
+  }
+  
+  
+  onSubmit() {
+    this.authService.login({
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    });
+  }
+  
+}
